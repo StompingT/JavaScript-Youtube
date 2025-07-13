08286386707064d3bc0c87d94bea68908111e35d
StompingT <treantstomping@gmail.com>
2025-07-13 17:01:36 +0000

    Y

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "Anudgd@gmail,com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState
+
+// accountId = 1  NOT Allowed  
+
+accountEmail = "anu@tbsm.com"
+accountPassword = "123456789"
+accountCity = "Japan"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+ */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // Treat all JS Code as newer version
+
+// alert(3+3) // we are using nodejs, not Browser
+
+console.log(1
+    +
+    1
+) // Code readabilty should be high
+
+let name = "Anay"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to ^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
